ff30fba903411eb8c8e9c116e22f922d34943a75
pawbibo <jesus.cabelin@bibo.com.ph>
2017-09-20 11:37:42 +0800

    Adding address

--- models/Store.js
+++ models/Store.js
@@ -19,7 +19,18 @@ const storeSchema = new mongoose.Schema({
     default: Date.now
   },
   location: {
-    
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [{
+      type: Number,
+      required: 'You must supply coordinates'
+    }],
+    address: {
+      type: String,
+      required: 'You must supply an address '
+    }
   }
 });
 
