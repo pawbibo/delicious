99f2bf43bbbe405e4b3c700f950cd0926a7e0dc8
pawbibo <jesus.cabelin@bibo.com.ph>
2017-09-22 11:35:16 +0800

    add store function

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -49,8 +49,13 @@ exports.createStore = async (req, res) => {
 
 exports.getStores = async (req, res) => {
   const stores = await Store.find();
-  console.log(stores);
   res.render('stores', { title: 'Stores', stores });
+};
+
+exports.getStoreBySlug = async (req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.slug });
+  if (!store) return next();
+  res.render('store', { store, title: store.name  });
 };
 
 exports.editStore = async (req, res) => {
