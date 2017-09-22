4aa7194d1f7b9350e1f302fa3e1eb7b6a7de1046
pawbibo <jesus.cabelin@bibo.com.ph>
2017-09-22 11:35:34 +0800

    add routes store

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
+
 router.get('/add', storeController.addStore);
 router.post('/add',
   storeController.upload,
