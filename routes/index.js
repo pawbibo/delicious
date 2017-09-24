587937da4b4bc147945a3b35a7962c205d486aec
pawbibo <jesus.cabelin@bibo.com.ph>
2017-09-24 23:12:12 +0800

    Add new route for login

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
+const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 router.get('/', catchErrors(storeController.getStores));
@@ -23,5 +24,7 @@ router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 router.get('/tags', catchErrors(storeController.getStoresByTag));
 // or /tags/*? which means optional
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
+
+router.get('/login', userController.loginForm);
 
 module.exports = router;
