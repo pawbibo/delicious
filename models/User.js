5b95a1c9aa920b2acd640e3c51a94de57444c03d
pawbibo <jesus.cabelin@bibo.com.ph>
2017-09-24 23:28:13 +0800

    Add user model

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const Promise = global.Promise;
+
+const md5 = require('md5');
+const validator = require('validator');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Invalid email address'],
+    require: 'Please enter email address'
+  },
+  name: {
+    type: String,
+    require: 'Please enter a name',
+    trim: true
+  }
+});
+
+userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+userSchema.plugin(mongodbErrorHandler);
+
+module.exports = mongoose.model('User', userSchema);
