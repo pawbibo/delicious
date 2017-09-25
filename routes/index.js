580e3ad171980d95159472fdb90dec79c4b6c9ed
pawbibo <jesus.cabelin@bibo.com.ph>
2017-09-25 10:32:01 +0800

    add registration validation

--- routes/index.js
+++ routes/index.js
@@ -27,5 +27,6 @@ router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
 router.get('/register', userController.registerForm);
+router.post ('/register', userController.validateRegister);
 
 module.exports = router;
