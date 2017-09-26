036a51517a6194d2d24bc9113bf5cfa0e03a8cc0
pawbibo <jesus.cabelin@bibo.com.ph>
2017-09-26 21:46:08 +0800

    Add token generation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 const passport = require('passport');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
 
 exports.login = passport.authenticate('local', {
   failureRedirect: '/login',
@@ -22,4 +25,23 @@ exports.isLoggedIn = (req, res, next) => {
 
   req.flash('error', 'Sorry, you need to be logged in to do that');
   res.redirect('/login');
+};
+
+exports.forgot = async (req, res) => {
+  // see if user email exists
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    req.flash('error', 'No account with that email exists');
+    return res.redirect('/login');
+  }
+  // set tokens and expiry to their account
+  user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+  user.resetPasswordExpires = Date.now() + 3600000; // 1 hour from now
+  await user.save();
+  // send email with token
+  const resetUrl = `https://${req.headers.host}/account/reset/${user.resetPasswordToken}`;
+  req.flash('success', `You have been emailed the password reset link ${resetUrl}`);
+  // redirect to the login page
+  res.redirect('/login');
+
 };
--- models/User.js
+++ models/User.js
@@ -20,7 +20,9 @@ const userSchema = new Schema({
     type: String,
     require: 'Please enter a name',
     trim: true
-  }
+  },
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
 });
 
 userSchema.virtual('gravatar').get(function() {
