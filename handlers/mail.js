2a27f308c85ed00aebd695231653abd9ce2a6a80
pawbibo <jesus.cabelin@bibo.com.ph>
2017-09-28 09:56:09 +0800

    Add text sending mail and inline css

--- handlers/mail.js
+++ handlers/mail.js
@@ -15,18 +15,20 @@ const transport = nodemailer.createTransport({
 
 const generateHTML = (filename, options = {}) => {
   const html = pug.renderFile(`${__dirname}/../views/email/${filename}.pug`, options);
-  return html;
+  const inlined = juice(html);
+  return inlined;
 };
 
 exports.send = async (options) => {
   const html = generateHTML(options.filename, options);
+  const text = htmlToText.fromString(html);
 
   const mailOptions = {
     from: 'Paw <noreply@gmail.com>',
     to: options.user.email,
     subject: options.subject,
-    html: html,
-    text: 'to be filled in'
+    html,
+    text
   };
 
   const sendMail = promisify(transport.sendMail, transport);
